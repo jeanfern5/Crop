c1bdc8bc2a4acdcd7d5585e618cef746af1842c5
Jeanette <jeanfern555@gmail.com>
2018-12-27 14:52:02 -0600

    Skeleton for one page web app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+
 import './App.css';
+import Nav from './components/Nav.js'
+// import Content from './components/Content.js'
+// import Footer from './components/Footer.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppContainer>
+        <Nav />
+        {/* <Content /> */}
+        {/* <Footer /> */}
+      </AppContainer>
     );
   }
 }
+
+const AppContainer = styled.div`
+  background: #061826;
+`;
 
 export default App;
---
+++ client/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Nav extends Component {
+    render() {
+        return(
+            <h1>Nav</h1>
+            // <NavContainer>
+          
+            // </NavContainer>
+        )
+    };
+};
+
+export default Nav;
