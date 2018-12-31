5a55a34d37651fe718388c43b297ac34f9499882
Jeanette <jeanfern555@gmail.com>
2018-12-31 14:20:42 -0600

    working on getting info icon popover to work, so far tooltip works

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { FaInfoCircle } from 'react-icons/fa';
+import { Button, Popover, PopoverHeader, PopoverBody, Tooltip } from 'reactstrap';
+// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+// import { Col, Row, Label, FormGroup, Input, DateTimeField } from 'reactstrap';
 
+  
+  
 class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tooltipOpen: false,
+            modal: false
+          };
+
+        this.toggle = this.toggle.bind(this);
+      };
+    
+      toggle() {
+        this.setState({
+          tooltipOpen: !this.state.tooltipOpen
+        });
+      };
+
+    //   toggle() {
+    //     this.setState({
+    //       modal: !this.state.modal
+    //     });
+    // };
+  
+
+  
+ 
     render() {
+        const classes = 'tooltip-inner'
+
         return(
             <GalleryContainer>
                 <GalleryIntro>
                     <h6>Gallery</h6>
-                    <FaInfoCircle/>
+
+                    {/* <Button onClick={this.toggle} type="button"><FaInfoCircle/></Button>
+                    <Popover style={{color:'red'}} trigger="focus" placement="bottom" isOpen={this.state.popoverOpen} target="PopoverFocus" toggle={this.toggle}>
+                        <PopoverHeader>Instructions</PopoverHeader>
+                        <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+                    </Popover> */}
+
+                      <p>Somewhere in here is a <span style={{textDecoration: "underline", color:"blue"}} href="#" id="TooltipExample">tooltip</span>.</p>
+        <Tooltip placement="right" isOpen={this.state.tooltipOpen} target="TooltipExample" toggle={this.toggle}>
+          Hello world!
+        </Tooltip>
+
+
+
+
                 </GalleryIntro>
                 <GalleryContent>Yo</GalleryContent>
                 <GalleryBar>
-                    <Button>Upload</Button>
-                    <Button>Export Image</Button>
-                    <Button>Export All</Button>
+                    <Buttons>Upload</Buttons>
+                    <Buttons>Export Image</Buttons>
+                    <Buttons>Export All</Buttons>
                 </GalleryBar>
             </GalleryContainer>
         );
@@ -51,7 +97,7 @@ const GalleryBar = styled.div`
     justify-content: space-between;
 `;
 
-const Button = styled.button`
+const Buttons = styled.button`
     background: #F27927;
     border-radius: 0.1875rem;
     font-size:13px;
